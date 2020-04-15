7d09e91e2a9b282263783c16a6cbb8d7fdfac806
mac <mac@MacBook-Pro-de-mac.local>
2020-04-15 12:30:46 -0500

    adding style loaders

--- webpack/webpack.development.config.js
+++ webpack/webpack.development.config.js
@@ -5,6 +5,14 @@ const baseConf = require('./webpack.base.config');
 module.exports = (env) => ({
   ...merge(baseConf(env), {
     mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          loader: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
     devServer: {
       contentBase: resolve(__dirname, '..', 'dist'),
       host: '0.0.0.0',
--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -1,8 +1,22 @@
 const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const baseConf = require('./webpack.base.config');
 
 module.exports = (env) => ({
   ...merge(baseConf(env), {
     mode: 'production',
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'assets/[name]_[hash].css',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          loader: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
   }),
 });
